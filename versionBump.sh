c14307e53b7deb7e562cd2446220654622733868
Harald Pehl <harald.pehl@gmail.com>
2017-02-22 17:29:06 +0100

    Change line endings

--- versionBump.sh
+++ versionBump.sh
@@ -1,12 +1,1 @@
-#!/bin/sh
-
-# Use this script to bump the version across POMs.
-
-PROGNAME=`basename "$0"`
-
-if [ "$#" -ne 1 ]; then
-    echo "Illegal number of arguments. Use '$PROGNAME <version>'"
-else
-    mvn versions:set -Pdocker -DnewVersion=$1
-    find . -name "*.versionsBackup" -exec rm {} \;
-fi
+#!/bin/sh# Use this script to bump the version across POMs.PROGNAME=`basename "$0"`if [ "$#" -ne 1 ]; then    echo "Illegal number of arguments. Use '$PROGNAME <version>'"else    mvn versions:set -Pdocker -DnewVersion=$1    find . -name "*.versionsBackup" -exec rm {} \;fi
