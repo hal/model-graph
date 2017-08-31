750392cc7d9308ff3d2f873ac675e1b5db3acbd8
Harald Pehl <harald.pehl@gmail.com>
2017-08-31 21:14:24 +0200

    docker images - wip

---
+++ docker/neo4j/push.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker push hpehl/model-graph-wildfly:latest
+docker push hpehl/model-graph-wildfly:11.0.0.CR1
+docker push hpehl/model-graph-wildfly:10.1.0.Final
+docker push hpehl/model-graph-wildfly:9.0.2.Final
