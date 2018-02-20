87fcd023785e0f1e3fab608eef0154dcf515690c
Harald Pehl <harald.pehl@gmail.com>
2018-02-20 21:23:54 +0100

    Get rid of nginx image (2)

--- docker/neo4j/build.sh
+++ docker/neo4j/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build --tag hpehl/model-graph-wildfly:11.0.0.Final --build-arg GRAPH_DB=wildfly-11.0.0.Final.tar.gz .
-docker build --tag hpehl/model-graph-wildfly:10.1.0.Final --build-arg GRAPH_DB=wildfly-10.1.0.Final.tar.gz .
-docker build --tag hpehl/model-graph-wildfly:9.0.2.Final  --build-arg GRAPH_DB=wildfly-9.0.2.Final.tar.gz  .
+docker build --tag hpehl/model-graph-wildfly:11.0.0.Final --build-arg GRAPH_DB=wildfly-11.0.0.Final.tar.gz --build-arg WILDFLY_VERSION=11 .
+docker build --tag hpehl/model-graph-wildfly:10.1.0.Final --build-arg GRAPH_DB=wildfly-10.1.0.Final.tar.gz --build-arg WILDFLY_VERSION=10 .
+docker build --tag hpehl/model-graph-wildfly:9.0.2.Final  --build-arg GRAPH_DB=wildfly-9.0.2.Final.tar.gz --build-arg WILDFLY_VERSION=9 .
