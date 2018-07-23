a3011c38cdf7482da81236c5b62cc496bcc7c83e
Harald Pehl <harald.pehl@gmail.com>
2018-07-23 10:23:41 +0200

    Remove WildFly 10.1.0, add WildFly 13.0.0

--- docker/neo4j/build.sh
+++ docker/neo4j/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
+docker build --tag hpehl/model-graph-wildfly:13.0.0.Final --build-arg GRAPH_DB=wildfly-13.0.0.Final.tar.gz --build-arg PLAY_URL=https://hal.github.io/model-graph/model-graph-13.html .
 docker build --tag hpehl/model-graph-wildfly:12.0.0.Final --build-arg GRAPH_DB=wildfly-12.0.0.Final.tar.gz --build-arg PLAY_URL=https://hal.github.io/model-graph/model-graph-12.html .
 docker build --tag hpehl/model-graph-wildfly:11.0.0.Final --build-arg GRAPH_DB=wildfly-11.0.0.Final.tar.gz --build-arg PLAY_URL=https://hal.github.io/model-graph/model-graph-11.html .
-docker build --tag hpehl/model-graph-wildfly:10.1.0.Final --build-arg GRAPH_DB=wildfly-10.1.0.Final.tar.gz --build-arg PLAY_URL=https://hal.github.io/model-graph/model-graph-10.html .
